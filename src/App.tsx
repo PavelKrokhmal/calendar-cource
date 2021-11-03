fa73e974c976aa6685ec77bc3aa7ef06d33226c7
Pavel <pavel.krokhmal.dev@gmail.com>
2021-11-03 10:39:48 +0200

    added AppRouter navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import AppRouter from "./components/AppRouter";
 
 function App() {
-  return (
-    <div>
-      Working...
-    </div>
-  );
+    return (
+        <AppRouter/>
+    );
 }
 
 export default App;
